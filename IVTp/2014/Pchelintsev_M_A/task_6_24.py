006d7e1c0565d2c6265f70cf866ce83fffac8654
Lolvax <mol.par.max@mail.ru>
2016-06-24 18:35:06 +0300

    Create task_6_24.py

---
+++ IVTp/2014/Pchelintsev_M_A/task_6_24.py
@@ -0,0 +1,9 @@
+import random
+loshadi = ['Гнедая','Рыжая','Серая','Вороная']
+otvet = input('')
+otvet = otvet.lower()
+otvet = otvet.capitalize()
+if otvet == random.choice(loshadi):
+	print('Верно!')
+elif:
+	print('Попробуйте еще раз
